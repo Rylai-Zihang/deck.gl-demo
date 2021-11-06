a1ca7611babd1116a917cd6587a45f2a010209df
Zihang He <rylaihzh@gmail.com>
2021-11-06 21:27:02 +0800

    add mapInstance by useRef

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -6,7 +6,7 @@ import { MapboxLayer } from "@deck.gl/mapbox"
 import store from "../../store"
 import { getInitialView } from "../../utils"
 import { InitialView } from "../../types"
-import { MAPBOX_TOKEN, POINT_COLORS } from '../../utils/constants'
+import { MAPBOX_TOKEN, POINT_COLORS, DEFAULT_INITIAL_VIEW } from '../../utils/constants'
 
 export default function Map() {
     const { state, dispatch } = useContext(store.Context)
@@ -15,59 +15,64 @@ export default function Map() {
 
     const deckRef = useRef<any>(null)
     const mapRef = useRef<any>(null)
-    const map = mapRef?.current?.getMap()
-    const deck = deckRef?.current?.deck
+    const mapInstanceRef = useRef<{ map: any }>({
+        map: null
+    })
 
     const initialView: InitialView =
         fileContent ?
             getInitialView(fileContent) :
-            { zoom: 12, longitude: 114.05, latitude: 22.55 }
+            DEFAULT_INITIAL_VIEW
 
 
     const onMapLoad = useCallback(() => {
+        const map = mapRef.current.getMap()
+        const deck = deckRef.current.deck
         // prevent flashing
-        map?.addLayer(
-            new MapboxLayer({ 'id': "empty-layer", deck })
+        map.addLayer(
+            new MapboxLayer({ id: "empty-layer", deck })
         )
+        mapInstanceRef.current.map = map
     }, [])
 
     useEffect(() => {
+        if(!fileContent) return
+        const { map } = mapInstanceRef.current
         // get the newest layer
         const newIndex = layerArray.length - 1
         const layerName = layerArray[newIndex]
-        if(fileContent) {
-            map.addSource(layerName, {
-                'type': 'geojson',
-                'data': fileContent
-            })
-            map.addLayer({
-                'id': `${layerName}-point`,
-                'type': 'circle',
-                'source': layerName,
-                'paint': {
-                    'circle-radius': 3,
-                    'circle-color': POINT_COLORS[newIndex]
-                },
-                'filter': ['==', '$type', 'Point']
-            })
-            map.addLayer({
-                'id': `${layerName}-label`,
-                'type': 'symbol',
-                'source': layerName,
-                'layout': {
-                    'text-field': ['get', 'name'],
-                    'text-size': 12,
-                    'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
-                    'text-radial-offset': 0.5,
-                    'text-justify': 'auto'
-                }
-            })
-        }
+        map.addSource(layerName, {
+            'type': 'geojson',
+            'data': fileContent
+        })
+        map.addLayer({
+            'id': `${layerName}-point`,
+            'type': 'circle',
+            'source': layerName,
+            'paint': {
+                'circle-radius': 3,
+                'circle-color': POINT_COLORS[newIndex]
+            },
+            'filter': ['==', '$type', 'Point']
+        })
+        map.addLayer({
+            'id': `${layerName}-label`,
+            'type': 'symbol',
+            'source': layerName,
+            'layout': {
+                'text-field': ['get', 'name'],
+                'text-size': 12,
+                'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+                'text-radial-offset': 0.5,
+                'text-justify': 'auto'
+            }
+        })
     }, [fileContent])
 
     useEffect(() => {
         if(!clickedLayer) return
         const { index, checked } = clickedLayer
+        const { map } = mapInstanceRef.current
         const visibilityState = checked ? 'visible' : 'none'
         const layerName = layerArray[index]
         map?.setLayoutProperty(
